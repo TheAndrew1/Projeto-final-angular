e802c4f15076f44ba45be3e057c64313eb51cee6
Andre <andre.velasco@itaipuparquetec.org.br>
2024-08-08 14:24:12 -0300

    estrutura do formulario feita

--- src/app/aluno/form/form.component.ts
+++ src/app/aluno/form/form.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
 export class FormComponent {
+  formulario = new FormGroup({
+    nome: new FormControl("", [Validators.required, Validators.minLength(3)]),
+    nota1: new FormControl(null, [Validators.required, Validators.min(0), Validators.max(10)]),
+    nota2: new FormControl(null, [Validators.required, Validators.min(0), Validators.max(10)])
+  });
 
+  botaoCadastrar:boolean = true;
 }
